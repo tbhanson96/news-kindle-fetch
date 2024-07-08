fad4d215002a4e7cfb796500c9df72ed0f7008e5
Tim Hanson <tbh20715@gmail.com>
2024-07-07 18:37:15 -0700

    add economist recipe

---
+++ src/book-utils.ts
@@ -0,0 +1,124 @@
+import { Calibre } from 'node-calibre';
+import nodemailer from 'nodemailer';
+import path from 'path';
+import { exec } from 'child_process';
+import util from 'util';
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
+
+
+const getFileInfo = (bookName: string) => {
+    const today = new Date();
+    const fileDate = `${today.getMonth()+1}-${today.getDate()}-${today.getFullYear()}`;
+    const outputFile = path.join('out', `${fileDate}-${bookName}.epub`);
+
+    return { fileDate, outputFile };
+}
+/**
+ * Run the economist recipe.
+ */
+const economist = async () => {
+    const calibre = new Calibre();
+    const { outputFile, fileDate } = getFileInfo('economist');
+    try {
+        console.log(`Starting fetch of latest ${fileDate} economist article...`);
+        await calibre.run("ebook-convert",
+            [
+                'economist.recipe',
+                outputFile,
+            ],
+        );
+        return outputFile;
+    } catch (err) {
+        console.log('Error occured:', err);
+    }
+}
+
+/**
+ * Run the NYTimes calibre recipe.
+ */
+const nyTimes = async () => {
+    const calibre = new Calibre();
+
+    const { outputFile, fileDate } = getFileInfo('nytimes');
+    try {
+        console.log('Creating cover image...');
+        await createNyTimesCover();
+        console.log(`Starting fetch of latest ${fileDate} NY times article...`);
+        await calibre.run("ebook-convert",
+            [
+                'nytimes.recipe',
+                outputFile,
+            ],
+        );
+        return outputFile;
+    } catch (err) {
+        console.log('Error occured:', err);
+    }
+}
+
+const sendEpubs = async (files: string[]) => {
+    const senderEmail = 'tbh20715@gmail.com';
+    const recipientEmail = 'tbhanson96@kindle.com';
+    const emailHost = 'smtp.gmail.com';
+    const emailPort = 465;
+
+    const clientId = process.env.OAUTH_ID;
+    const clientSecret = process.env.OAUTH_SECRET;
+    const refreshToken = process.env.OAUTH_REFRESH_TOKEN;
+
+    if (!clientId || !clientSecret || !refreshToken) {
+        console.log('Missing email OAUTH credentials, exiting..');
+        process.exit(1);
+    }
+
+    const mailer = nodemailer.createTransport({
+        host: emailHost,
+        port: emailPort,
+        secure: true,
+        auth: {
+            type: 'OAuth2',
+            user: senderEmail,
+            clientId,
+            clientSecret,
+            refreshToken,
+        },
+    });
+
+    for (const file of files) {
+        let options: SMTPTransport.Options = {
+            from: senderEmail,
+            to: recipientEmail,
+            attachments: [
+                {
+                    path: file,
+                },
+            ],
+        };
+        await new Promise<void>((res, rej) => {
+            mailer.sendMail(options, (err, info) => {
+                if (err) {
+                    console.log('Failed to send book to kindle:', err);
+                    rej(err);
+                } else {
+                    console.log('Successfully sent book to kindle!');
+                    res();
+                }
+            });
+        }); 
+    }
+};
+
+const createNyTimesCover = async () => {
+    const today = new Date();
+    let labelDate = today.toLocaleDateString("en-US", { year: 'numeric', month: 'short', day: 'numeric' });
+    const execAsync = util.promisify(exec);
+    try {
+        const { stdout, stderr } = await execAsync(`sh create-nytimes-cover.sh \"${labelDate}\" ${path.join('out', 'date.png')} ${path.join('out', 'ny-times-cover.png')}`);
+        console.log('imagemagick:', stdout);
+        console.error('imagemagick:', stderr);
+    } catch (e) {
+        console.error('Could not create NY Times cover image: ', e);
+    }
+};
+
+export { sendEpubs, nyTimes, economist };
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,33 @@
-import { downloadAndSend } from "./downloadAndSend";
-downloadAndSend(true);
+import { sendEpubs, nyTimes, economist } from "./book-utils";
+import commandLineArgs, { OptionDefinition } from 'command-line-args';
+
+const options: OptionDefinition[] = [
+  {
+    name: 'book', type: String, multiple: true, defaultOption: true, defaultValue: 'nytimes'
+  }
+];
+
+(async () => {
+  const results: string[] = [];
+  const args: any = commandLineArgs(options);
+
+  for (const b of args.book) {
+    switch(b) {
+      case 'economist': {
+        const result = await economist();
+        results.push(result!);
+        break;
+      }
+      case 'nytimes': {
+        const result = await nyTimes();
+        results.push(result!);
+        break;
+      }
+      default: {
+        throw new Error(`Unknown book option: ${b}`);
+      }
+    }
+  }
+
+  await sendEpubs(results);
+})();
